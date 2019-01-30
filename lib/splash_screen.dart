f74cf3f7f56baef1966d6120c9b2570454a86a10
why <894321963@163.com>
2019-01-30 21:34:13 +0800

    change image url

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -38,7 +38,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
     return FadeTransition(
       opacity: _animation,
       child: Image.network(
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548860385441&di=f32425e9dd39347c17812a0da8d72b2b&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F016a065625c70232f87557019e87f6.jpg%401280w_1l_2o_100sh.jpg',
+        '',
         scale: 2.0,
         fit: BoxFit.cover,
       ),
